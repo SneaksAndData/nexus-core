823db7a18b947110403ffc9a48b8de00a0eb91eb
George Zubrienko <gzu@ecco.com>
2024-12-12 18:06:48 +0100

    WIP port CQL stuff

--- pkg/checkpoint/request/metadata_store.go
+++ pkg/checkpoint/request/metadata_store.go
@@ -1,25 +1,52 @@
 package request
 
 import (
-	models2 "github.com/SneaksAndData/nexus-core/pkg/checkpoint/models"
+	"github.com/SneaksAndData/nexus-core/pkg/checkpoint/models"
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v3"
+	"k8s.io/klog/v2"
+	"os"
 )
 
 type MetadataStore interface {
-	UpsertMetadata(entry *models2.SubmissionBufferEntry) error
-	ReadMetadata(checkpoint *models2.CheckpointedRequest) (*models2.SubmissionBufferEntry, error)
+	UpsertMetadata(entry *models.SubmissionBufferEntry) error
+	ReadMetadata(checkpoint *models.CheckpointedRequest) (*models.SubmissionBufferEntry, error)
 }
 
 type CqlStore struct {
 	cluster    *gocql.ClusterConfig
-	cqlSession *gocqlx.Session
+	cqlSession gocqlx.Session
+	logger     klog.Logger
 }
 
-func (cqls *CqlStore) UpsertMetadata(entry *models2.SubmissionBufferEntry) error {
+func NewCqlStore(cluster *gocql.ClusterConfig, logger klog.Logger) *CqlStore {
+	session, err := gocqlx.WrapSession(cluster.CreateSession())
+	if err != nil {
+		logger.V(0).Error(err, "failed to create CQL session")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+	return &CqlStore{
+		cluster:    cluster,
+		cqlSession: session,
+		logger:     logger,
+	}
+}
+
+func NewAstraCqlStore(logger klog.Logger) *CqlStore {
+	bundleBytes := os.Getenv("NEXUS__CQL_ASTRA_BUNDLE")
+	if bundleBytes == "" {
+		logger.V(0).Info("Astra bundle or credentials are not present in the environment")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	// unpack and bootstrap Astra connection
 	return nil
 }
 
-func (cqls *CqlStore) ReadMetadata(checkpoint *models2.CheckpointedRequest) (*models2.SubmissionBufferEntry, error) {
+func (cqls *CqlStore) UpsertMetadata(entry *models.SubmissionBufferEntry) error {
+	return nil
+}
+
+func (cqls *CqlStore) ReadMetadata(checkpoint *models.CheckpointedRequest) (*models.SubmissionBufferEntry, error) {
 	return nil, nil
 }
