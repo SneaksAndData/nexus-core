97b2575e24e65441e18aba103df78482db7967ef
George Zubrienko <gzu@ecco.com>
2025-02-27 17:37:00 +0100

    Add error codes and cancellation model

---
+++ pkg/checkpoint/models/cancellation_request.go
@@ -0,0 +1,7 @@
+package models
+
+// CancellationRequest hold information on who and why cancelled the submission
+type CancellationRequest struct {
+	Initiator string `json:"initiator" binding:"required"`
+	Reason    string `json:"reason"  binding:"required"`
+}
