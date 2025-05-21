6882cde9a5f7894a092cb63c784f069884217678
George Zubrienko <gzu@ecco.com>
2025-05-21 17:51:56 +0200

    Use informer cache to instead of API calls

--- pkg/resolvers/resource_resolvers.go
+++ pkg/resolvers/resource_resolvers.go
@@ -2,16 +2,18 @@ package resolvers
 
 import (
 	"context"
-	v1 "github.com/SneaksAndData/nexus-core/pkg/apis/science/v1"
+	"fmt"
+	nexusv1 "github.com/SneaksAndData/nexus-core/pkg/apis/science/v1"
 	"github.com/SneaksAndData/nexus-core/pkg/checkpoint/models"
 	nexusclientset "github.com/SneaksAndData/nexus-core/pkg/generated/clientset/versioned"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 )
 
 // GetWorkgroupByRef reads NexusWorkgroup definition using the reference from the template
-func GetWorkgroupByRef(template *v1.NexusAlgorithmTemplate, client nexusclientset.Interface, resourceNamespace string) (*v1.NexusAlgorithmWorkgroupSpec, error) {
+func GetWorkgroupByRef(template *nexusv1.NexusAlgorithmTemplate, client nexusclientset.Interface, resourceNamespace string) (*nexusv1.NexusAlgorithmWorkgroupSpec, error) {
 	workgroup, err := client.ScienceV1().NexusAlgorithmWorkgroups(resourceNamespace).Get(context.TODO(), template.Spec.WorkgroupRef.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
@@ -27,8 +29,27 @@ func isNexusRun(objMeta *metav1.ObjectMeta) bool {
 	return objMeta.Labels[models.NexusComponentLabel] == models.JobLabelAlgorithmRun
 }
 
+// GetInformerObjectKey generates a key that can be used to look up a cached object representation from a respective informer
+func GetInformerObjectKey(resourceNamespace string, resourceName string) string {
+	return fmt.Sprintf("%s/%s", resourceNamespace, resourceName)
+}
+
+// GetCachedObject retrieves a cached NexusAlgorithmTemplate resource from informer cache
+func GetCachedObject[T any](objectName string, resourceNamespace string, informer cache.SharedIndexInformer) (*T, error) {
+	resource, exists, err := informer.GetStore().GetByKey(GetInformerObjectKey(resourceNamespace, objectName))
+	if err != nil {
+		return nil, err
+	}
+
+	if !exists {
+		return nil, nil
+	}
+
+	return resource.(*T), nil
+}
+
 // IsNexusRunEvent checks if an API server event is related to a Nexus run.
-func IsNexusRunEvent(event *corev1.Event, client kubernetes.Interface, resourceNamespace string) (bool, error) {
+func IsNexusRunEvent(event *corev1.Event, resourceNamespace string, informers map[string]cache.SharedIndexInformer) (bool, error) {
 	// ignore anything outside Nexus execution scope
 	if event.InvolvedObject.Namespace != resourceNamespace {
 		return false, nil
@@ -36,7 +57,7 @@ func IsNexusRunEvent(event *corev1.Event, client kubernetes.Interface, resourceN
 
 	switch event.InvolvedObject.Kind {
 	case "Job":
-		job, err := client.BatchV1().Jobs(resourceNamespace).Get(context.TODO(), event.InvolvedObject.Name, metav1.GetOptions{})
+		job, err := GetCachedObject[batchv1.Job](event.InvolvedObject.Name, resourceNamespace, informers[event.InvolvedObject.Kind])
 
 		if err != nil {
 			return false, err
@@ -44,7 +65,7 @@ func IsNexusRunEvent(event *corev1.Event, client kubernetes.Interface, resourceN
 
 		return isNexusRun(&job.ObjectMeta), nil
 	case "Pod":
-		pod, err := client.CoreV1().Pods(resourceNamespace).Get(context.TODO(), event.InvolvedObject.Name, metav1.GetOptions{})
+		pod, err := GetCachedObject[corev1.Pod](event.InvolvedObject.Name, resourceNamespace, informers[event.InvolvedObject.Kind])
 
 		if err != nil {
 			return false, err
