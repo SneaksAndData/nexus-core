f3b11621a3dfbf508134fa255a40246ae4668af9
George Zubrienko <gzu@ecco.com>
2025-08-11 14:49:23 +0200

    WIP

--- pkg/resolvers/resource_resolvers_test.go
+++ pkg/resolvers/resource_resolvers_test.go
@@ -3,18 +3,52 @@ package resolvers
 import (
 	"fmt"
 	nexusv1 "github.com/SneaksAndData/nexus-core/pkg/apis/science/v1"
+	"github.com/SneaksAndData/nexus-core/pkg/checkpoint/models"
 	nexusclient "github.com/SneaksAndData/nexus-core/pkg/generated/clientset/versioned"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	cache "k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2/ktesting"
 	"testing"
+	"time"
 )
 
 type fixture struct {
-	client nexusclient.Interface
+	client     nexusclient.Interface
+	kubeClient kubernetes.Interface
+
+	//kubeObjects  []runtime.Object
+	k8sFactory   kubeinformers.SharedInformerFactory
+	k8sInformers map[string]cache.SharedIndexInformer
 }
+
+func (f *fixture) populateJobs(jobs []batchv1.Job) *fixture {
+	for _, job := range jobs {
+		_ = f.k8sInformers["Job"].GetIndexer().Add(&job)
+	}
+
+	return f
+}
+
+func (f *fixture) populatePods(pods []corev1.Pod) *fixture {
+	for _, pod := range pods {
+		_ = f.k8sInformers["Pod"].GetIndexer().Add(&pod)
+	}
+
+	return f
+}
+
+var noResyncPeriodFunc = func() time.Duration { return 0 }
 
 func newFixture(t *testing.T) *fixture {
 	f := &fixture{}
+	f.k8sInformers = map[string]cache.SharedIndexInformer{}
+
 	kubeCfg, err := clientcmd.BuildConfigFromFlags("", "../../test-resources/kubecfg/controller")
 	if err != nil {
 		t.Fatal(fmt.Errorf("error building kubeConfig: %v", err))
@@ -25,6 +59,13 @@ func newFixture(t *testing.T) *fixture {
 	if err != nil {
 		t.Fatal(fmt.Errorf("error building Nexus client: %v", err))
 	}
+
+	f.kubeClient = fake.NewClientset()
+
+	f.k8sFactory = kubeinformers.NewSharedInformerFactoryWithOptions(f.kubeClient, noResyncPeriodFunc(), kubeinformers.WithNamespace("nexus"))
+
+	f.k8sInformers["Job"] = f.k8sFactory.Batch().V1().Jobs().Informer()
+	f.k8sInformers["Pod"] = f.k8sFactory.Core().V1().Pods().Informer()
 
 	return f
 }
@@ -60,5 +101,55 @@ func Test_GetExistingWorkgroupByRef(t *testing.T) {
 
 	if wg != nil && wg.Cluster != "kind-nexus-shard-0" {
 		t.Errorf("expected NexusAlgorithm workgroup to target kind-nexus-shard-0 cluster, but received %s", wg.Cluster)
+	}
+}
+
+func Test_IsNexusRunEvent(t *testing.T) {
+	_, ctx := ktesting.NewTestContext(t)
+	testFixture := newFixture(t).populateJobs([]batchv1.Job{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-job",
+				Namespace: "nexus",
+				Labels: map[string]string{
+					models.NexusComponentLabel: models.JobLabelAlgorithmRun,
+				},
+			},
+		},
+	},
+	)
+	testFixture.k8sFactory.Start(ctx.Done())
+
+	evt := &corev1.Event{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Event",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-event",
+			Namespace: "nexus",
+		},
+		InvolvedObject: corev1.ObjectReference{
+			Kind:      "Job",
+			Namespace: "nexus",
+			Name:      "test-job",
+		},
+		Reason:         "test",
+		Message:        "test",
+		Source:         corev1.EventSource{},
+		FirstTimestamp: metav1.Time{},
+		LastTimestamp:  metav1.Time{},
+		Count:          1,
+		EventTime:      metav1.MicroTime{},
+	}
+
+	isNexusRun, err := IsNexusRunEvent(evt, "nexus", testFixture.k8sInformers)
+
+	if err != nil {
+		t.Errorf("error checking an Event to be emitted from Nexus: %v", err)
+	}
+
+	if !isNexusRun {
+		t.Errorf("expected a test event to be emitted from Nexus")
 	}
 }
