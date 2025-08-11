8d32346780c3ec796af81c58627a600b6d7e079e
George Zubrienko <gzu@ecco.com>
2025-08-11 15:11:37 +0200

    WIP

--- pkg/resolvers/resource_resolvers.go
+++ pkg/resolvers/resource_resolvers.go
@@ -37,7 +37,7 @@ func GetInformerObjectKey(resourceNamespace string, resourceName string) string 
 // GetCachedObject retrieves a cached resource from informer cache
 func GetCachedObject[T any](objectName string, resourceNamespace string, informer cache.SharedIndexInformer) (*T, error) {
 	resource, exists, err := informer.GetStore().GetByKey(GetInformerObjectKey(resourceNamespace, objectName))
-	if err != nil {
+	if err != nil { // coverage-ignore
 		return nil, err
 	}
 
@@ -63,7 +63,7 @@ func IsNexusRunEvent(event *corev1.Event, resourceNamespace string, informers ma
 			return false, nil
 		}
 
-		if err != nil {
+		if err != nil { // coverage-ignore
 			return false, err
 		}
 
@@ -75,7 +75,7 @@ func IsNexusRunEvent(event *corev1.Event, resourceNamespace string, informers ma
 			return false, nil
 		}
 
-		if err != nil {
+		if err != nil { // coverage-ignore
 			return false, err
 		}
 
