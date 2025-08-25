2d6d7df0eb72a8b1b012b03856c45805e80c41b3
George Zubrienko <zubrienkog@gmail.com>
2025-08-25 15:06:43 +0200

    Remove unused model (#51)

--- pkg/checkpoint/models/cancellation_request.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// CancellationRequest hold information on who and why cancelled the submission
-type CancellationRequest struct {
-	Initiator string `json:"initiator" binding:"required"`
-	Reason    string `json:"reason"  binding:"required"`
-	RequestId string `json:"requestId" binding:"required"`
-}
