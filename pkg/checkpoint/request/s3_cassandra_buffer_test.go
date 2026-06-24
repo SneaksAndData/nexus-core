83d5e656243bc7aa518fa7ef9cd201b5cec082e8
George Zubrienko <gzu@ecco.com>
2026-06-24 12:49:05 +0200

    Add tests for bare (non-indexed) Cassandra store

--- pkg/checkpoint/request/s3_cassandra_buffer_test.go
+++ pkg/checkpoint/request/s3_cassandra_buffer_test.go
@@ -231,104 +231,114 @@ func TestDefaultBuffer_GetMetadata(t *testing.T) {
 }
 
 func TestDefaultBuffer_Add(t *testing.T) {
-	f := newFixture(t, newIndexedCassandraConfig())
+	cases := []struct {
+		name    string
+		fixture *fixture
+	}{
+		{"add to buffer that uses indexed Cassandra store", newFixture(t, newIndexedCassandraConfig())},
+		{"add to buffer that uses bare Cassandra store", newFixture(t, newBareCassandraConfig())},
+	}
 
-	go f.buffer.Start(nil)
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			go tc.fixture.buffer.Start(nil)
 
-	time.Sleep(1 * time.Second)
+			time.Sleep(1 * time.Second)
 
-	err := f.buffer.Add("new-id", "test-algorithm-v2", &models.AlgorithmRequest{
-		AlgorithmParameters: map[string]interface{}{
-			"parameterA": "a",
-			"parameterB": "b",
-		},
-		CustomConfiguration: nil,
-		RequestApiVersion:   "",
-		Tag:                 "",
-		ParentRequest: &models.AlgorithmRequestRef{
-			RequestId:     "test-parent",
-			AlgorithmName: "test-algorithm-v2",
-		},
-		PayloadValidFor: "24h",
-	}, &v1.NexusAlgorithmSpec{
-		Container: &v1.NexusAlgorithmContainer{
-			Image:              "test-image",
-			Registry:           "test",
-			VersionTag:         "v1.2.3",
-			ServiceAccountName: "test-sa",
-		},
-		ComputeResources: &v1.NexusAlgorithmResources{
-			CpuLimit:        "1000m",
-			MemoryLimit:     "2000Mi",
-			CustomResources: nil,
-		},
-		WorkgroupRef: &v1.NexusAlgorithmWorkgroupRef{
-			Name:  "default",
-			Group: "science.sneaksanddata.com/v1",
-			Kind:  "NexusAlgorithmWorkgroup",
-		},
-		Command: "python",
-		Args:    []string{"main.py", "--request-id=%s", "--sas-uri=%s"},
-		RuntimeEnvironment: &v1.NexusAlgorithmRuntimeEnvironment{
-			EnvironmentVariables: []corev1.EnvVar{
-				{
-					Name:  "TEST_ENV_VAR",
-					Value: "TEST_VALUE",
+			err := tc.fixture.buffer.Add("new-id", "test-algorithm-v2", &models.AlgorithmRequest{
+				AlgorithmParameters: map[string]interface{}{
+					"parameterA": "a",
+					"parameterB": "b",
 				},
-			},
-			MappedEnvironmentVariables: []corev1.EnvFromSource{
-				{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "test-secret",
+				CustomConfiguration: nil,
+				RequestApiVersion:   "",
+				Tag:                 "",
+				ParentRequest: &models.AlgorithmRequestRef{
+					RequestId:     "test-parent",
+					AlgorithmName: "test-algorithm-v2",
+				},
+				PayloadValidFor: "24h",
+			}, &v1.NexusAlgorithmSpec{
+				Container: &v1.NexusAlgorithmContainer{
+					Image:              "test-image",
+					Registry:           "test",
+					VersionTag:         "v1.2.3",
+					ServiceAccountName: "test-sa",
+				},
+				ComputeResources: &v1.NexusAlgorithmResources{
+					CpuLimit:        "1000m",
+					MemoryLimit:     "2000Mi",
+					CustomResources: nil,
+				},
+				WorkgroupRef: &v1.NexusAlgorithmWorkgroupRef{
+					Name:  "default",
+					Group: "science.sneaksanddata.com/v1",
+					Kind:  "NexusAlgorithmWorkgroup",
+				},
+				Command: "python",
+				Args:    []string{"main.py", "--request-id=%s", "--sas-uri=%s"},
+				RuntimeEnvironment: &v1.NexusAlgorithmRuntimeEnvironment{
+					EnvironmentVariables: []corev1.EnvVar{
+						{
+							Name:  "TEST_ENV_VAR",
+							Value: "TEST_VALUE",
 						},
 					},
+					MappedEnvironmentVariables: []corev1.EnvFromSource{
+						{
+							SecretRef: &corev1.SecretEnvSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: "test-secret",
+								},
+							},
+						},
+					},
+					Annotations:     nil,
+					DeadlineSeconds: ptr.Int32(300),
+					MaximumRetries:  ptr.Int32(10),
 				},
-			},
-			Annotations:     nil,
-			DeadlineSeconds: ptr.Int32(300),
-			MaximumRetries:  ptr.Int32(10),
-		},
-		ErrorHandlingBehaviour:     nil,
-		DatadogIntegrationSettings: &v1.NexusDatadogIntegrationSettings{MountDatadogSocket: ptr.Bool(true)},
-	}, &v1.NexusAlgorithmWorkgroupSpec{
-		Description:  "test",
-		Capabilities: nil,
-		Cluster:      "test-shard",
-		Tolerations:  nil,
-		Affinity:     nil,
-	}, &metav1.OwnerReference{
-		APIVersion: v1.SchemeGroupVersion.String(),
-		Kind:       "NexusAlgorithmTemplate",
-		Name:       "test-parent",
-		UID:        "test-parent-uid",
-	}, false)
+				ErrorHandlingBehaviour:     nil,
+				DatadogIntegrationSettings: &v1.NexusDatadogIntegrationSettings{MountDatadogSocket: ptr.Bool(true)},
+			}, &v1.NexusAlgorithmWorkgroupSpec{
+				Description:  "test",
+				Capabilities: nil,
+				Cluster:      "test-shard",
+				Tolerations:  nil,
+				Affinity:     nil,
+			}, &metav1.OwnerReference{
+				APIVersion: v1.SchemeGroupVersion.String(),
+				Kind:       "NexusAlgorithmTemplate",
+				Name:       "test-parent",
+				UID:        "test-parent-uid",
+			}, false)
 
-	if err != nil {
-		t.Fatalf("error when buffering checkpoint: %v", err)
-	}
+			if err != nil {
+				t.Fatalf("error when buffering checkpoint: %v", err)
+			}
 
-	time.Sleep(time.Second * 5)
+			time.Sleep(time.Second * 5)
 
-	checkpoint, err := f.buffer.Get("new-id", "test-algorithm-v2")
+			checkpoint, err := tc.fixture.buffer.Get("new-id", "test-algorithm-v2")
 
-	if err != nil {
-		t.Fatalf("error when reading an expected checkpoint entry: %v", err)
-	}
+			if err != nil {
+				t.Fatalf("error when reading an expected checkpoint entry: %v", err)
+			}
 
-	if checkpoint == nil {
-		t.Fatalf("expected checkpoint not found in the buffer after calling Add")
-	}
+			if checkpoint == nil {
+				t.Fatalf("expected checkpoint not found in the buffer after calling Add")
+			}
 
-	if checkpoint.LifecycleStage != models.LifecycleStageBuffered {
-		t.Fatalf("lifecycle stage should be Buffered, but is %s", checkpoint.LifecycleStage)
-	}
+			if checkpoint.LifecycleStage != models.LifecycleStageBuffered {
+				t.Fatalf("lifecycle stage should be Buffered, but is %s", checkpoint.LifecycleStage)
+			}
 
-	if checkpoint.Parent == nil {
-		t.Fatalf("parent should not be nil")
-	}
+			if checkpoint.Parent == nil {
+				t.Fatalf("parent should not be nil")
+			}
 
-	if checkpoint.Parent.RequestId != "test-parent" {
-		t.Fatalf("parent request id should be test-parent-uid, but is %s", checkpoint.Parent.RequestId)
+			if checkpoint.Parent.RequestId != "test-parent" {
+				t.Fatalf("parent request id should be test-parent-uid, but is %s", checkpoint.Parent.RequestId)
+			}
+		})
 	}
 }
