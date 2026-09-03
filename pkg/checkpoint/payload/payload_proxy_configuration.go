90cf8d06f5b5ece964988fe76dc5e53f43ef0e6b
George Zubrienko <gzu@ecco.com>
2026-09-03 17:00:08 +0200

    Generate actionable url and ensure no mistakes in path template

--- pkg/checkpoint/payload/payload_proxy_configuration.go
+++ pkg/checkpoint/payload/payload_proxy_configuration.go
@@ -1,7 +1,25 @@
 package payload
 
+import "strings"
+
 type RequestPayloadProxyConfiguration struct {
 	TenantId          string `mapstructure:"tenant-id"`
 	ServePathTemplate string `mapstructure:"serve-path-template"`
 	SignSecret        string `mapstructure:"sign-secret"`
+	ExternalName      string `mapstructure:"external-name"`
+	Insecure          bool   `mapstructure:"insecure"`
+}
+
+func (rpp *RequestPayloadProxyConfiguration) GetProxyScheme() string {
+	if rpp.Insecure {
+		return "http"
+	}
+	return "https"
+}
+
+func (rpp *RequestPayloadProxyConfiguration) GetServePathTemplate() string {
+	if rpp.ServePathTemplate == "" || strings.HasPrefix(rpp.ServePathTemplate, "/") {
+		return rpp.ServePathTemplate
+	}
+	return "/" + rpp.ServePathTemplate
 }
